d4deed6e1037d0e5fa2e9f2cbfeb227bafaccff4
Chris Crow <chrismcrow@gmail.com>
2018-08-20 13:43:50 -0700

    add css design elements

--- js/scripts.js
+++ js/scripts.js
@@ -15,6 +15,19 @@ ItemObject.prototype.move = function() {
 
 //front-end
 $(document).ready(function() {
+  //anime
+  // anime({
+  //   targets: 'div',
+  //   translateX: [
+  //     { value: 100, duration: 1200 },
+  //     { value: 0, duration: 800 }
+  //   ],
+  //   rotate: '1turn',
+  //   backgroundColor: '#FFF',
+  //   duration: 2000,
+  //   loop: true
+  // });
+
   $("form#new-entry").submit(function(event) {
     event.preventDefault();
 
