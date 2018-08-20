6c25c3f116198e65f12d81b7aca95bce1ce1383f
Chris Crow <chrismcrow@gmail.com>
2018-08-20 11:39:12 -0700

    let item move between lists depending on if it is checked or not.

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,15 @@ function ItemObject(item) {
   this.item = item;
 };
 
+ItemObject.prototype.move = function() {
+  $("input[name=checkbox]").bind("click", function() {
+    if ($(this).is(":checked")) {
+      $(this).parent().appendTo($("#completed"));
+    } else if ($(this).not(":checked")) {
+      $(this).parent().appendTo($("#to-do-list"));
+    }
+  });
+}
 
 //front-end
 $(document).ready(function() {
@@ -10,12 +19,12 @@ $(document).ready(function() {
     event.preventDefault();
 
     var input = $("input#new-item").val();
-    var newItem = new ItemObject(input)
+    var newItem = new ItemObject(input);
 
     $("div#to-do-list").append("<p><input type='checkbox' name='checkbox'>  " + newItem.item + "</p>")
-    $("input[name=checkbox]").bind("click", function() {
-      $(this).parent().appendTo($("#completed"));
-    });
+
+    newItem.move();
+
     $("input#new-item").val("");
   });
 });
